e8446cbb249480fef6f1ff66deb252a2c71d044c
zacklm9 <96926004+zacklm9@users.noreply.github.com>
2022-12-22 18:30:30 +0100

    Add files via upload

---
+++ inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,33 @@
+package org.sid.inventoryservice;
+
+import org.sid.inventoryservice.entities.Product;
+import org.sid.inventoryservice.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+@SpringBootApplication
+
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner start(ProductRepository productRepository, RepositoryRestConfiguration restConfiguration){
+		restConfiguration.exposeIdsFor(Product.class);
+		return args -> {
+			productRepository.save(new Product(null,"ordinateur",788,12));
+			productRepository.save(new Product(null,"imprimante",88,129));
+			productRepository.save(new Product(null,"smartphone",1200,203));
+			productRepository.findAll().forEach(p->{
+				System.out.println(p.getName());
+			});
+		};
+	}
+
+}
+
